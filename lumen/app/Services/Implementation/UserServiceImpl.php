ffd7a05847c6667f8355c4fde68b365811504b03
Vlad Hat <yosshimendieta94@gmail.com>
2021-11-25 00:57:45 -0500

    Configuracion de Implementation para la tabla user y token 2

---
+++ lumen/app/Services/Implementation/UserServiceImpl.php
@@ -0,0 +1,12 @@
+<?php 
+namespace App\Services\Implementation;
+use App\Services\Interfaces\IUserServiceInterface;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserServiceImpl implements IUserServiceInterface
+{
+	private $model;
+	
+}
+?>
